06aba4c27d11eea4991edb460abf02f6d431a0cf
asafchoo <45887109+asafchoo@users.noreply.github.com>
2021-03-02 09:47:25 +0200

    Update Thermocuple_MAX6675.ino

--- Thermocuple_MAX6675/Thermocuple_MAX6675.ino
+++ Thermocuple_MAX6675/Thermocuple_MAX6675.ino
@@ -1,3 +1,14 @@
+ /*
+ *  Author: Asaf Chertkoff (asaf.chertkoff at gmail.com)
+ *  SoC tested: ESP-12E 8266 nodeMCU 
+ *  
+ *  2/3/2021
+ *  Reference:
+ *  Configure nodeMCU:
+ *  https://create.arduino.cc/projecthub/electropeak/getting-started-w-nodemcu-esp8266-on-arduino-ide-28184f 
+ *  
+ */
+
 #include <max6675.h>
 #include <Wire.h>
 
